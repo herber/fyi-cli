a9b363171463af2d7f2c190161d0058e36510a7b
tobihrbr <tobihrbr@gmail.com>
2017-05-27 15:48:22 +0200

    add cli

---
+++ cli.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const init = require('./');
+
+console.log('  ___           _  ');
+console.log(' / __)         (_) ');
+console.log('| |__   _   _   _  ');
+console.log('|  __) | | | | | | ');
+console.log('| |    | |_| | | | ');
+console.log('|_|     \\__  | |_| ');
+console.log('        (____/     ');
+console.log('');
+
+init();
